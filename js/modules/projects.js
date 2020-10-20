3a19b7c87e1bd92d662bb4a019b636392a2f18e2
Xuan Hien Nguyen <hien1020@Xuans-MacBook-Air.local>
2020-10-19 20:42:33 -0400

    add functionality

---
+++ js/modules/projects.js
@@ -0,0 +1,19 @@
+export default {
+    Housecom: {
+        title: "HouseCom Thermostat",
+        description: "HouseCom™ ThermoSecurity™ System is a fully digital, hardwired and wireless enabled home personal and secure environment control system In this project, I developed the brand look and marketing strategies to promote the products.",
+        category: "Branding | 3D Modeling"
+    },
+
+    Nutrilite: {
+        title: "Nutrilite",
+        description: "Designing brochure for Nutrilite™ sale team to inform the importance of taking vitamins to improve immunity system.",
+        cagegory: "Marketing"
+    },
+
+    Eco: {
+        title: "Eco Culinarius",
+        description: "Eco Culinarius is a healthy fast vegeterian food restaurant providing high-quality, fresh and healthy food for your health. In this project, we developed the brand look and an app for online shipping.",
+        cagegory: "Branding | User Interaction"
+    }
+}
